04e3f5f572c29452e6c8c8ecc5e30fa83bf4d8dd
jaone <juanmarquesz145@gmail.com>
2025-02-15 12:28:07 -0300

    COMEÇANDO O PROJETO

--- .vscode/meu-primeiro-projeto-java/.vscode/Projeto java teste/src/App.java
+++ .vscode/meu-primeiro-projeto-java/.vscode/Projeto java teste/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola mundo como estao.");
+        System.out.println("vMAOS COMEÇAR COM O PROJETO A PARTIR DE HOJE CERTO, VVAMOS ESPEERAR O RESTO DA TURMA CHEGAR PARA C=NOS COMERÇAMOS.");
     }
 }
