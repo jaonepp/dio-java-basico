49ed6f8d71f660c853e187e8262ec20b701e3ae2
Juan <juanmarquesz145@gmail.com>
2025-03-22 15:23:02 -0300

    seila

--- .vscode/meu-primeiro-projeto-java/.vscode/Projeto java teste/src/App.java
+++ .vscode/meu-primeiro-projeto-java/.vscode/Projeto java teste/src/App.java
@@ -3,3 +3,7 @@ public class App {
         System.out.println("vMAOS COMEÇAR COM O PROJETO A PARTIR DE HOJE CERTO, VVAMOS ESPEERAR O RESTO DA TURMA CHEGAR PARA C=NOS COMERÇAMOS.");
     }
 }
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Aqui esta TODOS oss materiais scolares dessse ano bom, vamos ver qual sera as novas coisas para esse ano");
+    }
--- .vscode/meu-primeiro-projeto-java/src/App.java
+++ .vscode/meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Bem vindos turma dio");
+        System.out.println("jony e bot");
     }
 }
+local jhony ("picture") 
+        public = static: ("jhony É bot sim ou nao?")
+        wait (3)
+        public = static: ("sim")
+       while
+       public = sun.awt.util (-
+   
+       ) {
+        
+       }
+    
+        button getClass() 
